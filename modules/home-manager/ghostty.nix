1d037a9a95708c10dd905fcce96c92db5f3a8ad8
mef_love <meflov3r@gmail.com>
2025-07-10 14:45:15 +0700

    test test test

--- modules/home-manager/ghostty.nix
+++ modules/home-manager/ghostty.nix
@@ -11,18 +11,16 @@
       theme = "catppuccin-mocha"; # Пример темы
       font-size = 10;
       #... другие настройки Ghostty
-    };
-
-    # Привязки клавиш [17]
-    keybindings = {
-      "super+c" = "copy_to_clipboard";
-      "super+v" = "paste_from_clipboard";
-      "super+shift+h" = "goto_split:left";
-      "super+shift+j" = "goto_split:bottom";
-      "super+shift+k" = "goto_split:top";
-      "super+shift+l" = "goto_split:right";
-      "ctrl+page_up" = "jump_to_prompt:-1";
-      #...
+      keybindings = {
+        "super+c" = "copy_to_clipboard";
+        "super+v" = "paste_from_clipboard";
+        "super+shift+h" = "goto_split:left";
+        "super+shift+j" = "goto_split:bottom";
+        "super+shift+k" = "goto_split:top";
+        "super+shift+l" = "goto_split:right";
+        "ctrl+page_up" = "jump_to_prompt:-1";
+        #...
+      };
     };
 
     # Очистка стандартных привязок клавиш, если вы хотите полностью кастомные [17]
