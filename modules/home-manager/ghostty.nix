288763f9389f2a5151de09a2da1e673c1a308566
mef_love <meflov3r@gmail.com>
2025-07-12 00:14:44 +0700

    test test test

--- modules/home-manager/ghostty.nix
+++ modules/home-manager/ghostty.nix
@@ -1,24 +1,4 @@
 { config, pkgs, ... }:
 {
-  programs.ghostty = {
-    enable = true;
-    # package = null; # Ghostty пока не в nixpkgs
-
-    settings = {
-      theme = "catppuccin-mocha";
-      font-size = 10;
-      "shell-integration" = "fish";
-    };
-
-    extraConfig = ''
-      [keybindings]
-      "super+c" = "copy_to_clipboard"
-      "super+v" = "paste_from_clipboard"
-      "super+shift+h" = "goto_split:left"
-      "super+shift+j" = "goto_split:bottom"
-      "super+shift+k" = "goto_split:top"
-      "super+shift+l" = "goto_split:right"
-      "ctrl+page_up" = "jump_to_prompt:-1"
-    '';
-  };
+  # programs.ghostty.enable = true;
 }
--- users/common/default.nix
+++ users/common/default.nix
@@ -1,9 +1,39 @@
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, lib, inputs, ... }:
+
+let
+  # Путь к вашим dotfiles
+  dotfilesDir = ../../dotfiles;
+
+  # Функция для рекурсивного поиска файлов и создания атрибутов
+  mapDirectory = dir:
+    let
+      items = builtins.readDir dir;
+      files = lib.attrsets.mapAttrs' (name: type:
+        if type == "regular" then
+          lib.attrsets.nameValuePair name (dir + "/${name}")
+        else
+          lib.attrsets.nameValuePair name null
+      ) items;
+      dirs = lib.attrsets.mapAttrs' (name: type:
+        if type == "directory" then
+          lib.attrsets.nameValuePair name (mapDirectory (dir + "/${name}"))
+        else
+          lib.attrsets.nameValuePair name null
+      ) items;
+    in
+      files // lib.mapAttrs (name: value: lib.mapAttrs (n: v: "${name}/${v}") value) dirs;
+
+  # Создаем атрибуты для xdg.configFile
+  configFiles = lib.mapAttrs'
+    (name: value: lib.attrsets.nameValuePair name { source = value; })
+    (lib.filterAttrs (n: v: v != null) (lib.attrsets.collapse (mapDirectory (dotfilesDir + "/config"))));
+
+in
 {
   # Импорт всех модулей Home Manager
   imports = [
     inputs.self.modules.home-manager.fish
-    inputs.self.modules.home-manager.ghostty
+    # inputs.self.modules.home-manager.ghostty # Отключено, так как управляется файлом
     inputs.self.modules.home-manager.hyprland
     inputs.self.modules.home-manager.programs
   ];
@@ -23,6 +53,6 @@
   # Настройки для XDG Base Directory Specification
   xdg = {
     enable = true;
-    # configFile."mimeapps.list".source = "${config.xdg.configHome}/mimeapps.list";
+    configFile = configFiles;
   };
 }
