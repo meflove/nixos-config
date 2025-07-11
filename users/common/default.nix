1a28f77ad69fb6330d904587e46ed0c19bba0147
mef_love <meflov3r@gmail.com>
2025-07-11 23:56:01 +0700

    test test test

--- users/common/default.nix
+++ users/common/default.nix
@@ -1,26 +1,12 @@
 { config, pkgs, inputs, ... }:
 {
   # Импорт всех модулей Home Manager
-  # imports = [
-  #   inputs.self.modules.home-manager.fish
-  #   inputs.self.modules.home-manager.ghostty
-  #   inputs.self.modules.home-manager.hyprland
-  #   inputs.self.modules.home-manager.programs
-  # ];
-
-  programs.git = {
-    enable = true;
-    userName = "meflove";
-    userEmail = "meflov3r@gmail.com";
-  };
-
-  programs.gpg = {
-    enable = true;
-  };
-  services.gpg-agent = {
-    enable = true;
-    enableSshSupport = true;
-  };
+  imports = [
+    inputs.self.modules.home-manager.fish
+    inputs.self.modules.home-manager.ghostty
+    inputs.self.modules.home-manager.hyprland
+    inputs.self.modules.home-manager.programs
+  ];
 
   # Общие настройки Home Manager
   home = {
