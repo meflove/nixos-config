839d1de9e70464defeab759123feb41090c9ed57
meflove <meflov3r@icloud.com>
2025-08-17 18:00:17 +0700

    add some virtualization (podman virt-manager)

---
+++ modules/nixos/podman.nix
@@ -0,0 +1,28 @@
+{ pkgs, ... }: {
+  virtualisation.podman = {
+    enable = true;
+
+    dockerCompat = true;
+    dockerSocket.enable = true;
+
+    defaultNetwork.settings.dns_enabled = true;
+  };
+
+  environment.variables.DBX_CONTAINER_MANAGER = "podman";
+  users.extraGroups.podman.members = [ "xnm" ];
+
+  environment.systemPackages = with pkgs; [
+    nvidia-docker
+    nerdctl
+
+    distrobox
+    qemu
+    lima
+
+    podman-compose
+    podman-tui
+
+    docker-compose
+    lazydocker
+  ];
+}
