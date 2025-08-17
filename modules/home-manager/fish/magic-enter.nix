d39d5224a746cce215017362f141f5fce3d9c199
meflove <meflov3r@icloud.com>
2025-08-17 08:09:51 +0700

    fix magic-enter

--- modules/home-manager/fish/magic-enter.nix
+++ modules/home-manager/fish/magic-enter.nix
@@ -3,18 +3,15 @@
     body = ''
       function magic-enter-cmd --description "Issue git status or ls on hitting enter in a dir"
         set -l cmd ll
-        set -l is_git_repository (fish -c "git rev-parse --is-inside-work-tree 2>/dev/null" | grep true)
-        set -l in_root_folder (fish -c "git rev-parse --show-toplevel 2>/dev/null" | grep (pwd))
-        set -l repo_has_changes (fish -c "git status -s --ignore-submodules=dirty 2>/dev/null")
+        set -l is_git_repository (git rev-parse --is-inside-work-tree 2>/dev/null)
 
         if test -n "$is_git_repository"
-          if test -n "$in_root_folder"
-            if test -n "$repo_has_changes"
-              set cmd "ll && g status"
-            end
+          set -l git_status (git status --porcelain -s)
+
+          if test -n "$git_status"
+            set cmd "ll && g status"
           end
         end
-
         echo $cmd
       end
 
