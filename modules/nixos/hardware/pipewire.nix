2e419b62f694dd03480d8f30c9c07c1a6e1454f5
meflove <meflov3r@icloud.com>
2025-09-09 03:02:38 +0700

    feat(pipewire): configure upmixing and clock rates
    Add explicit clock rate and quantum settings.
    Enable channel upmixing for stereo sources.

--- modules/nixos/hardware/pipewire.nix
+++ modules/nixos/hardware/pipewire.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 {
   security.rtkit.enable = true;
   services.pulseaudio.enable = false;
@@ -14,13 +14,41 @@
       enable = true;
     };
 
-    extraConfig.pipewire."92-low-latency" = {
-      "context.properties" = {
-        "default.clock.rate" = 48000;
-        "default.clock.quantum" = 512; # ~12ms
-        "default.clock.min-quantum" = 512;
-        "default.clock.max-quantum" = 512;
+    extraConfig = {
+      pipewire = {
+        "20-no-resampling" = {
+          "context.properties" = {
+            "default.clock.rate" = 48000;
+            "default.clock.allowed-rates" = [
+              44100
+              48000
+              96000
+              192000
+            ];
+          };
+        };
+
+        "10-sound" = {
+          "context.properties" = {
+            "default.clock.min-quantum" = 512;
+            "default.clock.quantum" = 4096;
+            "default.clock.max-quantum" = 8192;
+          };
+        };
       };
+
+      pipewire-pulse."20-upmix" = {
+        # Enables upmixing
+        "stream.properties" = {
+          "channelmix.upmix" = true;
+          "channelmix.upmix-method" = "psd"; # none, simple
+          "channelmix.lfe-cutoff" = 150;
+          "channelmix.fc-cutoff" = 12000;
+          "channelmix.rear-delay" = 12.0;
+        };
+      };
+
+      client."20-upmix" = config.services.pipewire.extraConfig.pipewire-pulse."20-upmix";
     };
   };
 }
