d39ad653a049e2cd32d8dda71c00f3ba9c5730cd
meflove <meflov3r@icloud.com>
2025-09-30 02:56:52 +0700

    chore(git): add extensive git configuration

--- modules/home-manager/development/git.nix
+++ modules/home-manager/development/git.nix
@@ -9,15 +9,51 @@
     userEmail = "meflov3r@icloud.com";
 
     extraConfig = {
+      core = {
+        editor = "nvim";
+        whitespace = "error";
+        preloadindex = true;
+      };
+
       init = {
         defaultBranch = "main";
       };
 
+      diff = {
+        renames = "copies";
+        interHunkContext = 10;
+      };
+
       pager.diff = "diffnav";
+
+      pull = {
+        default = "current";
+        rebase = true;
+      };
 
       push = {
         autoSetupRemote = true;
         default = "current";
+      };
+
+      rebase = {
+        autoStash = true;
+        missingCommitsCheck = "warn";
+      };
+
+      submodule = {
+        fetchJobs = 16;
+      };
+
+      log = {
+        abbrevCommit = true;
+      };
+
+      status = {
+        branch = true;
+        short = true;
+        showStash = true;
+        showUntrackedFiles = "all";
       };
     };
   };
